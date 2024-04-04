9c8e59683777a7d4fb64993dbdeb750b3175f1d3
RylanRRB <rylan.baboelal@gmail.com>
2024-04-04 10:29:12 +0200

    Model profile

--- dodo/base/models.py
+++ dodo/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+    city = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
