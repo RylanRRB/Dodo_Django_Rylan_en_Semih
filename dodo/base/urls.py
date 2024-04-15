a747bcac1dea9efc005a09a8dd7352c4eadec754
Sqmih <SemihALtinokSener@gmail.com>
2024-04-15 20:03:33 +0200

    halverwege af met menubalk/style/kleur/login/register

--- dodo/base/urls.py
+++ dodo/base/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.say_hello, name = "start_pagina"),
+    path("", views.say_firstname, name = "start_pagina"),
     path("", include('django.contrib.auth.urls')),
-    path("hello/", views.say_hello)
+    path("startpagina/", views.say_firstname),
+    path("pagina_twee/", views.say_lastname, name = "pagina_twee"),
 ]
--- dodo/base/views.py
+++ dodo/base/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def say_hello(request):
+def say_firstname(request):
     context = {"first_name": "Rylan en Semih"}
-    return render(request, "base/hello.html", context)
+    return render(request, "base/startpagina.html", context)
+
+def say_lastname(request):
+    context = {"last_name": "Baboelal en Sener"}
+    return render(request, "base/pagina_twee.html", context)
